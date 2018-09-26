e1321e67ba486a8a0099de171f0323f9981303e9
richardverheyen <richard.verheyen@gmail.com>
2018-09-26 16:29:24 +1000

    fix(config/prod.ex): put settings in the right places

--- config/prod.exs
+++ config/prod.exs
@@ -15,18 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :introduc, IntroducWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "infinite-ravine-36934.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info,
 
 config :introduc, Introduc.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
-
-# Do not print debug messages in production
-config :logger, level: :info,
 
 # ## SSL Support
 #
@@ -68,6 +69,3 @@ config :logger, level: :info,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-
-url: [scheme: "https", host: "infinite-ravine-36934.herokuapp.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
